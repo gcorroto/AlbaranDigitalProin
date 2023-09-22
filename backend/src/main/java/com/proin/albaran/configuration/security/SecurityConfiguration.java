1476386b9a33aa0ceedffbfab4b3a63c2911ae7b
Hugo Martinez Jara <hmartinez@profile.es>
2023-09-22 05:46:39 +0200

    Eliminamos referencias a oauth2

--- backend/src/main/java/com/proin/albaran/configuration/security/SecurityConfiguration.java
+++ backend/src/main/java/com/proin/albaran/configuration/security/SecurityConfiguration.java
@@ -1,12 +1,10 @@
 package com.proin.albaran.configuration.security;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import com.proin.albaran.configuration.annotation.CsrfQualifier;
 import com.proin.albaran.configuration.annotation.InsecuredQualifier;
-import com.proin.albaran.configuration.annotation.Oauth2Qualifier;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,8 +14,6 @@ import lombok.extern.slf4j.Slf4j;
 @AllArgsConstructor
 public class SecurityConfiguration {
 
-    // @Autowired(required = false)
-    @Oauth2Qualifier() // no funciona aún
     @CsrfQualifier()
     @InsecuredQualifier()
     private final SecurityConfigurationContainer httpSecurity;
@@ -27,7 +23,7 @@ public class SecurityConfiguration {
         log.debug("prepare configuration with security");
 
         if(httpSecurity == null) {
-            throw new Exception("debe definir un profile de seguridad en la aplicación 'dev' o 'pro' acompañado del perfil de seguridad 'csrf' , 'oauth2' o 'insecure' por ejemplo [spring.profiles.active=dev-insecure]");
+            throw new Exception("debe definir un profile de seguridad en la aplicación 'dev' o 'pro' acompañado del perfil de seguridad 'csrf' o 'insecure' por ejemplo [spring.profiles.active=dev-insecure]");
         }
         return httpSecurity.getSecurity().build();
     }
